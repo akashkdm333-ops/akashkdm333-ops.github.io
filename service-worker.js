d5ea4ceff01a346e63a57d05526ff2f8bc474e81
AKASH KADAM <akashkdm.333@gmail.com>
2026-01-28 16:47:07 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = "gst-calculator-v1";
+const CACHE_NAME = "gst-calculator-v2";
+
 const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json"
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js",
+  "./manifest.json"
 ];
 
 self.addEventListener("install", event => {
@@ -19,7 +20,9 @@ self.addEventListener("activate", event => {
     caches.keys().then(keys =>
       Promise.all(
         keys.map(key => {
-          if (key !== CACHE_NAME) return caches.delete(key);
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
         })
       )
     )
@@ -29,6 +32,9 @@ self.addEventListener("activate", event => {
 
 self.addEventListener("fetch", event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then(response => {
+      return response ||
+        fetch(event.request).catch(() => caches.match("./index.html"));
+    })
   );
 });
