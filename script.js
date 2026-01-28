f51192eccb8cbd1b398d760f3a20a85309fd209a
AKASH KADAM <akashkdm.333@gmail.com>
2026-01-28 15:38:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -225,3 +225,9 @@ document.addEventListener('DOMContentLoaded', () => {
 
   init();
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js");
+  });
+}
