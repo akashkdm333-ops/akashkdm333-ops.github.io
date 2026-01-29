5154c40770c96f4a7d29410de5b1a4c33111feaa
AKASH KADAM <akashkdm.333@gmail.com>
2026-01-29 14:15:08 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,42 +1,7 @@
-const CACHE_NAME = "gst-calculator-v2";
-
-const FILES_TO_CACHE = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/manifest.json",
-  "/icons/icon-192.png",
-  "/icons/icon-512.png"
-];
-
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
-  );
+self.addEventListener("install", e=>{
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.map(key => {
-          if (key !== CACHE_NAME) {
-            return caches.delete(key);
-          }
-        })
-      )
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener("fetch", event => {
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      return response ||
-        fetch(event.request).catch(() => caches.match("./index.html"));
-    })
-  );
+self.addEventListener("activate", e=>{
+  clients.claim();
 });
