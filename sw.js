0030a7efaeb4e99b4071a71387abb387b7faad71
AKASH KADAM <akashkdm.333@gmail.com>
2026-01-29 16:07:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,20 @@
-self.addEventListener("install", e=>{
-  self.skipWaiting();
+const CACHE = "gst-cache-v1";
+
+self.addEventListener("install", e => {
+e.waitUntil(
+caches.open(CACHE).then(c =>
+c.addAll([
+"/",
+"/index.html",
+"/style.css",
+"/script.js"
+])
+)
+);
 });
 
-self.addEventListener("activate", e=>{
-  clients.claim();
+self.addEventListener("fetch", e => {
+e.respondWith(
+caches.match(e.request).then(r => r || fetch(e.request))
+);
 });
