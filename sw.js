b2e03aaf473637a6b217a2465e90cff57212c095
AKASH KADAM <akashkdm.333@gmail.com>
2026-01-29 16:56:12 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,33 +1,32 @@
-const CACHE_NAME = "gst-calculator-v1";
+const CACHE = "gst-v1";
 
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./style.css",
-  "./script.js"
+const FILES = [
+  "/",
+  "/index.html",
+  "/style.css",
+  "/script.js"
 ];
 
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE).then(cache => cache.addAll(FILES))
   );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
+self.addEventListener("activate", e => {
+  e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        keys.map(k => k !== CACHE && caches.delete(k))
       )
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then(res => res || fetch(event.request))
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
   );
 });
-
