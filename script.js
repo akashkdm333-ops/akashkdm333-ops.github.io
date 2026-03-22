44e519fa27d12551f6f726e45cda7481023ff68f
AKASH KADAM <akashkdm.333@gmail.com>
2026-03-22 11:36:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const amountInput = document.getElementById("amount");
+const amountInput = document.getElementById("amount")
 const rateSelect = document.getElementById("rate");
 const typeSelect = document.getElementById("type");
 const calcBtn = document.getElementById("calc");
